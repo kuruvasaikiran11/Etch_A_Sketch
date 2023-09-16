b3afd86fe4f15924eacca283c9fd4f7aafa1e89a
kuruvasaikiran11 <kuruvasaikiran11@gmail.com>
2023-09-16 15:15:39 +0530

    Eraser Functionality Added to erase colors

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const gridContainer = document.getElementById("grid-container");
 const resetButton = document.getElementById("reset-button");
-const maxInteractions = 10; //Number of interactions to set full black
+const maxInteractions = 10; // Maximum number of interactions to reach full black
+const eraserButton = document.getElementById("eraser-button"); // Add an eraser button
+let erasing = false; // Flag to indicate eraser mode
 
 function createGrid(size) {
     gridContainer.innerHTML = "";
 
-    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`; // Set the number of columns
-    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`; // Set the number of rows
+    // Calculate the number of rows and columns based on viewport height and desired square size
+    const viewportHeight = window.innerHeight;
+    const squareSize = Math.floor(viewportHeight / size);
+
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, ${squareSize}px)`; // Set the number of columns
+    gridContainer.style.gridTemplateRows = `repeat(${size}, ${squareSize}px)`; // Set the number of rows
 
     for (let i = 0; i < size * size; i++) {
         const square = document.createElement("div");
@@ -22,10 +28,15 @@ function addHoverEffect() {
     const squares = document.querySelectorAll(".square");
     squares.forEach((square) => {
         square.addEventListener("mouseover", () => {
-            const interactions = parseInt(square.dataset.interactions);
-            if (interactions < maxInteractions) {
-                randomizeAndDarkenColor(square, interactions);
-                square.dataset.interactions = interactions + 1; // Increment interaction count
+            if (erasing) {
+                // If eraser mode is enabled, change square color to white
+                square.style.backgroundColor = "white";
+            } else {
+                const interactions = parseInt(square.dataset.interactions);
+                if (interactions < maxInteractions) {
+                    randomizeAndDarkenColor(square, interactions);
+                    square.dataset.interactions = interactions + 1; // Increment interaction count
+                }
             }
         });
     });
@@ -38,7 +49,7 @@ function randomizeAndDarkenColor(square, interactions) {
     const g = parseInt(rgbValues[1]);
     const b = parseInt(rgbValues[2]);
 
-    // Random RGB values
+    // Generate random RGB values
     const randomR = Math.floor(Math.random() * 256);
     const randomG = Math.floor(Math.random() * 256);
     const randomB = Math.floor(Math.random() * 256);
@@ -46,7 +57,7 @@ function randomizeAndDarkenColor(square, interactions) {
     // Calculate the darken factor (10% per interaction)
     const darkenFactor = (interactions + 1) / maxInteractions;
 
-    // Calculate the new RGB values by adding the random color and darkening
+    // Calculate the new RGB values by blending the random color and darkening
     const newR = Math.max(0, r * (1 - darkenFactor) + randomR * darkenFactor);
     const newG = Math.max(0, g * (1 - darkenFactor) + randomG * darkenFactor);
     const newB = Math.max(0, b * (1 - darkenFactor) + randomB * darkenFactor);
@@ -71,5 +82,15 @@ resetButton.addEventListener("click", () => {
         createGrid(newSizeInt);
     } else {
         alert("Please enter a valid number between 1 and 100.");
+    }
+});
+
+// Toggle eraser mode
+eraserButton.addEventListener("click", () => {
+    erasing = !erasing; // Toggle the eraser flag
+    if (erasing) {
+        eraserButton.textContent = "Eraser On"; // Update button text
+    } else {
+        eraserButton.textContent = "Eraser Off"; // Update button text
     }
 });
